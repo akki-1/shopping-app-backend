b22d3d43600d9db24542be1e7f0855e65e7a2ceb
Akshay Navgankar <navgankarakshay@gmail.com>
2022-08-28 16:22:18 +0530

    renamed to org name

--- src/main/java/com/shoppingApp/app/Shopping_Backend.java
+++ src/main/java/com/shoppingApp/app/Shopping_Backend.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Shopping_Backend {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Shopping_Backend.class, args);
 	}
 
 }
